c55989725890c05ae928abff7325de2f8f7d6af2
baranau_an <andrei_baranau1@epam.com>
2022-02-01 13:00:04 +0300

    Added project to repo.

---
+++ ConstrainedRegions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+
+namespace ConstrainedRegions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Code preparation in the finally block
+            RuntimeHelpers.PrepareConstrainedRegions();
+
+            try
+            {
+                Console.WriteLine("In try!");
+            }
+            finally
+            {
+                // Implicit call of the static constructor Type1
+                Type1.M();
+            }
+        }
+    }
+
+    public sealed class Type1
+    {
+        static Type1()
+        {
+            // In case of exception M is not called
+            Console.WriteLine("Type1's static ctor called");
+        }
+
+        // Using attribute, defined in the namespace
+        // System.Runtime.ConstrainedExecution
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        public static void M() { }
+    }
+}
